983852bf0509deb2c5dda0e16d2e9939ea033f52
Sean Tolbert <stolbert4150@gmail.com>
2022-06-27 16:44:32 -0400

    stubbing simple header-footer-object format

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AppBar } from '@mui/material'
+
+export default function Header() {
+  return (
+    // <AppBar>
+    //     Header
+    // </AppBar>
+    <div>
+        Header
+    </div>
+  )
+}
---
+++ src/components/NoteLoading.jsx
@@ -0,0 +1,5 @@
+export default function NoteLoading() {
+  return (
+    <div>NoteLoading</div>
+  )
+}
